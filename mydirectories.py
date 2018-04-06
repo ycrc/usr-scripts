a0ff955fcdfb7c71d2da7ded04db5e73a74ed19c
Kaylea Nelson <kaylea.nelson@yale.edu>
2018-04-06 15:12:46 -0400

    fix for users without either grace or omega directories

--- mydirectories.py
+++ mydirectories.py
@@ -9,16 +9,38 @@ metagroup, group, user = home.split('/')[-3:]
 print "Full directory paths for {}".format(user)
 print "These directories are available from both Grace and Omega\n"
 
-print 'Omega'
-print '====='
-print'{0:9} /gpfs/loomis/home.omega/{0}/{1}/{2}'.format('home', metagroup, group, user)
-print '{0:9} /gpfs/loomis/scratch.omega/{0}/{1}/{2}'.format('scratch', metagroup, group, user)
-print ' '
-print 'Grace'
-print '====='
-print '{0:9} /gpfs/loomis/home.grace/{1}/{2}/{3}'.format('home', metagroup, group, user)
-print '{0:9} /gpfs/loomis/project/{1}/{2}/{3}'.format('project', metagroup, group, user)
-print '{0:9} /gpfs/loomis/scratch60/{1}/{2}/{3}'.format('scratch60', metagroup, group, user)
+dirs = {}
+
+dirs['omega'] = {'home': '/gpfs/loomis/home.omega/{0}/{1}/{2}'.format(metagroup, group, user),
+                        'scratch': '/gpfs/loomis/home.omega/{0}/{1}/{2}'.format(metagroup, group, user)}
+
+dirs['grace'] = {'home': '/gpfs/loomis/home.grace/{0}/{1}/{2}'.format(metagroup, group, user),
+                        'project': '/gpfs/loomis/project/{0}/{1}/{2}'.format(metagroup, group, user),
+                        'scratch60': '/gpfs/loomis/scratch60/{0}/{1}/{2}'.format(metagroup, group, user)
+                        }
+
+
+
+if os.path.exists(dirs['omega']['home']) or os.path.exists(dirs['omega']['scratch']):
+    print 'Omega'
+    print '====='
+    if os.path.exists(dirs['omega']['home']):
+        print'{0:9} {1}'.format('home', dirs['omega']['home'])
+    if os.path.exists(dirs['omega']['scratch']):
+        print'{0:9} {1}'.format('scratch', dirs['omega']['scratch'])
+    print ' '
+
+if (os.path.exists(dirs['grace']['home']) or os.path.exists(dirs['grace']['project']) or
+    os.path.exists(dirs['grace']['scratch60'])):
+
+    print 'Grace'
+    print '====='
+    if os.path.exists(dirs['grace']['home']):
+        print'{0:9} {1}'.format('home', dirs['grace']['home'])
+    if os.path.exists(dirs['grace']['project']):
+        print'{0:9} {1}'.format('project', dirs['grace']['project'])
+    if os.path.exists(dirs['grace']['scratch60']):
+        print'{0:9} {1}'.format('scratch60', dirs['grace']['scratch60'])
 
 
 
